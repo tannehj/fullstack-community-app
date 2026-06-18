a63b758a9a839ed6a32ff3a1ae5808b770764471
Tanneh Jah <tannehjah@gmail.com>
2026-06-18 18:52:35 -0400

    connect frontend to Flask API

--- script.js
+++ script.js
@@ -6,29 +6,56 @@ let submitButton =document.getElementById("submitButton");
 let list= document.getElementById("storyList");
 let nameInput =document.getElementById("userName");
 let charCounter = document.getElementById("charCounter");
+let errorMessage = document.getElementById("errorMessage");
 
 
-let savedStories = localStorage.getItem("storiesList");
+//let savedStories = localStorage.getItem("storiesList");
+
+//Frontend asks Flask for stories
+//Flask returns JSON
+//Frontend displays them
+
+fetch("http://127.0.0.1:5000/stories")
+      .then (function(response)
+      {
+          return response.json();
+      })
+      .then(function(data){
+          storiesList=data;
+
+     if (storiesList.length > 0) {
+        storyId = Math.max(...storiesList.map(function(item) {
+            return item.id;
+        })) + 1;
+    }
+      
+
+      for (let storyObject of storiesList){
+        displayStory(storyObject);
+      }
+
+
+     });
 console.log("I reached the load section");
 
-if (savedStories !== null) {
-    storiesList = JSON.parse(savedStories);
-}
+//if (savedStories !== null) {
+   // storiesList = JSON.parse(savedStories);
+//}
 
-if (storiesList.length > 0) {
-    storyId = Math.max(...storiesList.map(function(item) {
-        return item.id;
-    })) + 1;
+//if (storiesList.length > 0) {
+   // storyId = Math.max(...storiesList.map(function(item) {
+       // return item.id;
+   // })) + 1;
 
 
      
-}
+//}
 console.log("storyId after load:", storyId);
 
 
     
-for (let storyObject of storiesList)
-      {displayStory(storyObject)}
+//for (let storyObject of storiesList)
+    //  {displayStory(storyObject)}
 
 
 
@@ -53,23 +80,35 @@ submitButton.addEventListener("click", function()
     }
         
     
-    let storyObject = {id: storyId, name:name, story:story };
+    let storyObject = {id: storyId, name:name, 
+        story:story };
     storyId++;
 
-    storiesList.push(storyObject);
-    console.log("After add:", [...storiesList]);
-    localStorage.setItem("storiesList", JSON.stringify(storiesList));
+    //storiesList.push(storyObject);
+    // console.log("After add:", [...storiesList]);
+    // localStorage.setItem("storiesList", JSON.stringify(storiesList));
    
-    //calling function
-    displayStory(storyObject);
+    // //calling function
+    // displayStory(storyObject);
     
-    
+    fetch("http://127.0.0.1:5000/stories",{
+        method:"POST",
+        headers: {"Content-Type": "application/json"},
+        body:JSON.stringify(storyObject)
+    })
+    .then( function(response){
+        return response.json();
+    })
+    .then (function(data){
+          storiesList.push(storyObject);
+          displayStory(storyObject);
+    });
 
     //let listItem =document.createElement("li");
     //let deleteButton = document.createElement("button")
 
     //listItem.textContent= storyObject.name + ":" + storyObject.story; //this is opening 
-    // the object box and extracting the name ans story attached to that id
+    // the object box and extracting the name and story attached to that id
     //deleteButton.textContent = "DELETE"
 
 

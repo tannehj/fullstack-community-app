e7a7db50208d7254b8a73e11a1b1475d0928359c
Tanneh Jah <tannehjah@gmail.com>
2026-06-19 22:17:40 -0400

    Load stories from SQLite as objects

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,25 @@
+import sqlite3 #database
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app= Flask(__name__)
 CORS(app)
+
+
+#database file create 
+def init_db():
+     conn=sqlite3.connect("stories.db")
+     cursor=conn.cursor()
+     cursor.execute("""
+        CREATE TABLE IF NOT EXISTS stories (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            story TEXT NOT NULL
+        )
+    """)
+     conn.commit()
+     conn.close()
+     
 
 #json test stories
 
@@ -14,14 +31,46 @@ stories=[{"id":1,
 
 @app.route("/stories", methods=["GET"])
 def get_stories():
-    return jsonify(stories)
+
+    conn=sqlite3.connect("stories.db")
+    cursor=conn.cursor()
+
+    cursor.execute("SELECT * FROM stories")
+
+    rows=cursor.fetchall()
+
+    conn.close()
+    stories_list = []
+
+    for row in rows:
+        stories_list.append({
+            "id": row[0],
+            "name": row[1],
+            "story": row[2]
+        })
+    
+    return jsonify(stories_list)
 
 
 @app.route("/stories", methods=["POST"])
 def create_story():
+     
      data=request.get_json()
-     stories.append(data)
-     return jsonify({"message": "Story added"})
+     conn=sqlite3.connect("stories.db")
+     cursor=conn.cursor()
+
+     cursor.execute(
+    "INSERT INTO stories (name, story) VALUES (?, ?)",
+    (data["name"], data["story"])
+)
+     conn.commit()
+
+     conn.close()
+
+     return jsonify({"message": "Story inserted"})
+     #stories.append(data)
+     
 
 if __name__== "__main__":
+     init_db()
      app.run(debug=True, port=5000)

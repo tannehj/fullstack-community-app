6e8e8b75479ddf3fb0199a0bfefa2c98420ac8d5
Tanneh Jah <tannehjah@gmail.com>
2026-07-03 22:00:04 -0400

    Add login page frontend

---
+++ frontend/login.js
@@ -0,0 +1,47 @@
+const username =document.getElementById("username");
+const password =document.getElementById("password");
+const loginMessage=document.getElementById("login-message");
+const loginForm = document.getElementById("login-form");
+
+loginForm.addEventListener("submit", appLogin);
+
+async function appLogin(event){
+    event.preventDefault();
+
+    const usernameValue=username.value.trim();
+    const passwordValue =password.value.trim();
+
+    if (usernameValue===""|| passwordValue==="")
+    {
+        loginMessage.textContent="Username or password is missing.";
+        return;
+    }
+
+           const loginObject= { 
+            username:usernameValue,
+            password:passwordValue
+                };
+
+    try {
+        const response=await fetch ("http://127.0.0.1:5000/login",{
+        method:"POST",
+         headers:{"Content-Type": "application/json"},
+            body: JSON.stringify(loginObject)
+        })
+         const data= await response.json();
+
+        if (!response.ok) {
+        loginMessage.textContent = data.message;
+        return;
+        }
+      
+        window.location.href = "stories.html";
+        
+    }catch (error) {
+    console.error(error); // Developer information thats me !!aw
+
+    loginMessage.textContent =
+        "Unable to connect to the server. Please try again later.";
+}
+
+}
